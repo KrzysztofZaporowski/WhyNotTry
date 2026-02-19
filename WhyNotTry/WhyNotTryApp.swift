b1d8246a24b70e13a74c8fde160f0fc9dbbab077
KrzysztofZaporowski <krzysztofzaporowski@gmail.com>
2026-02-19 19:39:29 +0100

    Initial Commit

---
+++ WhyNotTry/WhyNotTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhyNotTryApp.swift
+//  WhyNotTry
+//
+//  Created by Krzysztof Zaporowski on 19/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct WhyNotTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
