8ee8180ee126f423729e8df9f740a72e4c87c5d9
KrzysztofZaporowski <krzysztofzaporowski@gmail.com>
2026-02-19 20:12:51 +0100

    Basic functionality

--- WhyNotTry/ContentView.swift
+++ WhyNotTry/ContentView.swift
@@ -8,9 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    var activities = ["Archery", "Baseball", "Basketball", "Bowling", "Boxing", "Cricket", "Curling", "Fencing", "Golf", "Hiking", "Lacrosse", "Rugby", "Squash"]
+    var colors = [Color.red, .blue, .purple, .orange, .yellow, .green, .cyan, .mint]
+    
+    @State var selected = "Baseball"
+    
     var body: some View {
         VStack {
-            Text("Hello, SwiftUI!")
+            Text("Why not try...")
+                .font(.largeTitle.bold())
+            Circle()
+                .fill(colors.randomElement() ?? Color.blue)
+                .padding()
+                .overlay(
+                    Image(systemName: "figure.\(selected.lowercased())")
+                        .font(.system(size: 144))
+                        .foregroundColor(.white)
+                )
+            Text("\(selected)!")
+                .font(.title)
+            Button("Try again") {
+                selected = activities.randomElement() ?? "Archery"
+            }
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.roundedRectangle(radius: 12))
         }
         .padding()
     }
